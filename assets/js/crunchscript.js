b2a95b6647a44f04f73dafd0707609192ec9783d
Lucas Overland <lucas.overland@gmail.com>
2015-04-16 20:34:50 -0500

    addes some javascript

---
+++ assets/js/crunchscript.js
@@ -0,0 +1,14 @@
+---
+---
+//run crunch stuff.
+(function($, noisy){
+  $(document).ready(function() {
+    $('body').noisy({
+      'intensity' : 1,
+      'size' : 200,
+      'opacity' : 0.074,
+      'fallback' : '',
+      'monochrome' : true
+    }).css('background-color', '#262626');
+  });
+});
