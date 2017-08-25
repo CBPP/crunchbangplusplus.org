1e03c7a8f837c46fa2c7c9a8289d69c3f5d52aec
Lucas Overland <loverland@linkedin.com>
2017-08-25 15:31:00 -0500

    removed console logs

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-import "github.com/gopherjs/gopherjs/js"
 import "honnef.co/go/js/dom"
 
 func main() {
@@ -19,7 +18,6 @@ func lightboxImage(evt dom.Event) {
   mask := d.CreateElement("div")
   mask.SetAttribute("style", "transition: opacity 0.15s ease-in")
   mask.Class().SetString("fixed top-0 left-0 w-100 h-100 bg-black-90 flex items-center justify-center o-0 pointer")
-  js.Global.Get("console").Call("log", mask)
   
   img := d.CreateElement("img")
   img.SetAttribute("src", target.GetAttribute("src"))
