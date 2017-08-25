8fb02006cbdd00f94c4b872ee9a59a4d4ac46824
Lucas Overland <loverland@linkedin.com>
2017-08-25 15:05:59 -0500

    Now done in golang

--- main.go
+++ main.go
@@ -13,12 +13,32 @@ func main() {
 
 func lightboxImage(evt dom.Event) {
   evt.PreventDefault()
-  d := dom.GetWindow().Document()
+   d := dom.GetWindow().Document()
+  target := evt.Target()
   
   mask := d.CreateElement("div")
   mask.SetAttribute("style", "transition: opacity 0.15s ease-in")
   mask.Class().SetString("fixed top-0 left-0 w-100 h-100 bg-black-90 flex items-center justify-center o-0 pointer")
   js.Global.Get("console").Call("log", mask)
+  
   img := d.CreateElement("img")
-  img.SetAttribute("src", evt.Target())
+  img.SetAttribute("src", target.GetAttribute("src"))
+  img.Class().Add("w-80")
+  
+  // dom.GetWindow().SetTimeout(func() {
+  // }, 0)
+  mask.AppendChild(img)
+  
+  body := d.(dom.HTMLDocument).Body()
+  body.AppendChild(mask)
+  body.Class().Add("overflow-hidden")
+  dom.GetWindow().SetTimeout(func() {
+    mask.Class().Remove("o-0")
+  }, 0)
+  
+  mask.AddEventListener("click", true, func(evt2 dom.Event) {
+    evt2.PreventDefault()
+    mask.ParentNode().RemoveChild(mask)
+    body.Class().Remove("overflow-hidden")
+  })
 }
