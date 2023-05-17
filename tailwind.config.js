4a49910bd0760c7615d9bbcca60082d15986c057
Lucas <loverlan@linkedin.biz>
2023-05-16 21:42:09 -0500

    index2 test

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index2.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // require('@tailwindcss/typography'),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/container-queries'),
+  ],
+}
